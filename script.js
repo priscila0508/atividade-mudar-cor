cd31371d50caf41600c837002e8ef72fb04e14d2
EA - Priscila Andrade Gonçalves Pereira <priscila_andrade@educadventista.org>
2026-08-14 16:08:02 -0300

    Finalização hahaha

--- script.js
+++ script.js
@@ -4,11 +4,16 @@ const button = document.querySelector("button")
 button.addEventListener("click", adicionarClasseAoH1)
 
 function adicionarClasseAoH1(){
-    h1.classeList.add("cor-diferente")
 
     if(h1.classList.contains("cor-diferente")){
-        button.innerText = "Voltar ao original"
-    } else {
+        h1.classList.remove("cor-diferente")
+
+        document.body.classList.remove("cor-diferente")
         button.innerText = "Mudar cor"
+    } else {
+
+        h1.classList.add("cor-diferente")
+        document.body.classList.add("cor-diferente")
+        button.innerText = "Voltar ao original"
     }
 }
